87f8054aaa67f8975acf614ae34fbc627adaef70
Fernando Guedes <fernando.jobs@live.com>
2022-05-14 01:36:07 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/example/curso/config/TestConfig.java
+++ src/main/java/com/example/curso/config/TestConfig.java
@@ -53,6 +53,17 @@ public class TestConfig implements CommandLineRunner {
         List<Product> products = Arrays.asList(p1, p2, p3, p4, p5);
         productRepository.saveAll(products);
 
+        // Associar categorias aos produtos.
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+
+        List<Product> productsAndCategories = Arrays.asList(p1, p2, p3, p4, p5);
+        productRepository.saveAll(productsAndCategories);
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
